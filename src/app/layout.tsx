59727737ec4c71cbbf1ed400648e4af6f60c8043
AnkitTiwari2005 <ankitt6.2005@gmail.com>
2026-05-13 23:13:13 +0530

    revert: restore AdSense to original unconditional loading in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,6 +110,12 @@ export default function RootLayout({
           data-domain={ENV.PLAUSIBLE_DOMAIN}
           src="https://plausible.io/js/script.js"
         />
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9948709371742259"
+          crossOrigin="anonymous"
+        />
         {/* Google Analytics */}
         <script
           async
